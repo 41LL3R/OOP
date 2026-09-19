0abc1c24b509a5ab35c2efa9aa3da4c03faf519c
41LL3R <41LL3R@users.noreply.github.com>
2026-09-19 05:11:07 +0000

    Deploying to gh-pages from @ 41LL3R/OOP@18c3fdc91da8e0a61e4f1027b50fe0fce6d5bbd7 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.etronin.task111","l":"Sort"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.etronin.task111","l":"Main"},{"p":"ru.nsu.etronin.task111","l":"Sort"}];updateSearchResults();
