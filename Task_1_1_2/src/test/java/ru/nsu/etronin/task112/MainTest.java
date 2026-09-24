4a6f2fece79572d69e98f763b52a084ea76ea8bd
41LL3R <aletregor@gmail.com>
2026-09-24 20:59:29 +0700

    tests and other 2

---
+++ Task_1_1_2/src/test/java/ru/nsu/etronin/task112/MainTest.java
@@ -0,0 +1,13 @@
+package ru.nsu.etronin.task112;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void testMainConstructor() {
+        Main main = new Main();
+        assertNotNull(main);
+    }
+}
